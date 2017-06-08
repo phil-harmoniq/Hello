8deac5f680d9e3c7630f3862bad17a365d412e9a
Phil H <ph-hawkins@outlook.com>
2017-06-08 18:13:10 -0400

    More detailed info

--- Program.cs
+++ Program.cs
@@ -10,23 +10,24 @@ namespace ArgTest
         public static bool IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
         public static bool IsMacOS = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
         public static bool IsLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
-        public static string Arch = RuntimeInformation.OSArchitecture.ToString();
+        public static string Info = RuntimeInformation.OSDescription;
         public static string OS = GetOSTag();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("\n---------- Hello from C#/.NET Core! ----------");
-            Console.WriteLine($"Looks like you're using {OS}");
-            if (args != null && args.Length > 0)
+            Console.WriteLine("\n------------ Hello from C#/.NET Core! ------------");
+            Console.WriteLine($"Looks like you're using {OS}! More detailed info:");
+            Console.WriteLine($"{Info}");
+            if (args.Length > 0)
                 Console.WriteLine($"Arguments detected: {string.Join(", ", args)}");
-            Console.WriteLine("----------------------------------------------\n");
+            Console.WriteLine("--------------------------------------------------\n");
         }
 
         public static string GetOSTag()
         {
-            if (IsWindows) return $"Windows {Environment.OSVersion.Version}";
-            if (IsMacOS) return $"MacOS {Environment.OSVersion.Version}";
-            if (IsLinux) return $"Linux {Environment.OSVersion.Version}";
+            if (IsWindows) return "Windows";
+            if (IsMacOS) return "macOS";
+            if (IsLinux) return "Linux";
             return "Unknown";
         }
     }
